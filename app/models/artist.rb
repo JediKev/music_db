639295bdd11098bb903e5b3fae4043a22090075e
Kevin Thorne <kevinthorne7@yahoo.com>
2016-04-27 10:53:18 -0500

    Adds validations to models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,23 @@
 class Artist < ActiveRecord::Base
   belongs_to :genre
   has_many :songs
+
+  validates :name, presence: true, length: { minimum: 1 }
+  validates :genre_id, presence: true
+  #  This is to say the associated genre actually exists
 end
+
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  genre_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_artists_on_genre_id  (genre_id)
+#
